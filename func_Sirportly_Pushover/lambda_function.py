3609cf0f64239ad2460ab8d50d6bafefbeea6cac
Rob Hooper <rob@dogsbody.com>
2023-09-05 13:06:23 +0100

    feat: initial commit of Sirportly function

--- func_Sirportly_Pushover/lambda_function.py
+++ func_Sirportly_Pushover/lambda_function.py
@@ -1,22 +1,36 @@
-#import json
-#from urllib import unquote
-#from config import user_keys, sirportly_key
-#from pushover import send_pushover
-#
-#def lambda_handler(event, context):
-#    if 'ticket' in event:
-#        ticket_data = json.loads(unquote(event['ticket']))
-#        if ticket_data['user'] == None: # Sirportly sends two requests, one with no valid user.
-#            return(json.dumps({'Message': 'Invalid data'}))
-#        user_data = ticket_data['user']
-#        user = user_data['username']
-#        message = 'Update on: '+ticket_data['subject'].replace('+', ' ')+', Ticket: 'ticket_url+ticket_data['reference']
-#        params = {'token': sirportly_key, 'user': user_keys[user], 'message': message}
-#        if send_pushover(params) == True:
-#            print('Pushover Sent!')
-#            return(json.dumps({'Message': 'Pushover sent'}))
-#        else:
-#            return(json.dumps({'Message': 'Pushover failed, check CloudWatch logs'}))
-#    else:
-#        return(json.dumps({'Message': 'Invalid data'}))
+import json
+import boto3
+from urllib import unquote
 
+from Pushover import get_pushover_user_key, send_pushover
+
+# Fetch Sirportly key
+ssm = boto3.client('ssm')
+sirportly_key = ssm.get_parameter(Name='SirportlyKey',WithDecryption=True)['Parameter']['Value']
+
+def lambda_handler(event, context):
+	# Sanitise input
+	if 'ticket' not in event:
+		return(json.dumps({'Message': 'Invalid data'}))
+
+	ticket_data = json.loads(unquote(event['ticket']))
+
+	# Sirportly sends two requests, one with no valid user.
+	if ticket_data['user'] == None:
+		return(json.dumps({'Message': 'Invalid data'}))
+
+	user_data = ticket_data['user']
+	user = user_data['username']
+	subject = ticket_data['subject'].replace('+', ' ')
+	url = ticket_url + ticket_data['reference']
+
+	message = f'Update on: {subject}, Ticket: {url}'
+
+	params = {'token': sirportly_key, 'user': get_pushover_user_key(user), 'message': message}
+
+	# send_pushover(token: str, user_key: str, message: str, priority = 0: int)
+	if send_pushover(sirportly_key, user_keys, message) == True:
+		print('Pushover Sent!')
+		return(json.dumps({'Message': 'Pushover sent'}))
+	else:
+		return(json.dumps({'Message': 'Pushover failed, check CloudWatch logs'}))
