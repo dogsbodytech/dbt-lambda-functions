67df63b3b38db548c98ea4ca62ea6e72ba8820ac
Rob Hooper <rob@dogsbody.com>
2023-09-05 13:06:02 +0100

    feat: initial commit of Pushover library

--- includes/Pushover.py
+++ includes/Pushover.py
@@ -1,46 +1,41 @@
-
-# This code needs to be re-written using either urllib or urllib3 instead of requests
-
-# The old code was being passed one argument, 
-# I think we should be passing multiple arguments this time
-# At minimum then I think we use be passed...
-#  - application key
-#  - user/group key
-#  - message
-#  - priority (should default to 0/none if not set)
-# ... this is based on the feilds we have used in the past
-# We can add functionality like title and image attachments in the future if we need it.
-
+# Library and functions to interact with the Pushover API
 # Details of the API can be found at https://pushover.net/api#messages
 
+import http.client
+import urllib
+import boto3
 
-# This is the example that Pushover provide from
-# https://support.pushover.net/i44-example-code-and-pushover-libraries#python
-# 
-#import http.client, urllib
-#conn = http.client.HTTPSConnection("api.pushover.net:443")
-#conn.request("POST", "/1/messages.json",
-#  urllib.parse.urlencode({
-#    "token": "APP_TOKEN",
-#    "user": "USER_KEY",
-#    "message": "hello world",
-#  }), { "Content-type": "application/x-www-form-urlencoded" })
-#conn.getresponse()
+ssm = boto3.client('ssm')
 
+def get_pushover_user_key(username: str) -> str:
+	"""
+	Fetch pushover user key from SSM Parameter Store.
+	"""
+	user_keys = ssm.get_parameter(Name='PushoverUsers',WithDecryption=False)['Parameter']['Value']
+	return json.loads(user_keys)[user]
 
-# This is the old code from our old Lambda function...
-#
-#import requests
-#
-#def send_pushover(params):
-#    try:
-#        r = requests.post('https://api.pushover.net/1/messages.json', params, timeout=30)
-#        if r.status_code != requests.codes.ok:
-#            raise requests.exceptions.RequestException
-#        else:
-#            return(True)
-#    except requests.exceptions.RequestException:
-#        print('Error occured sending pushover with the following params:')
-#        print(params)
-#        return(False)
+def send_pushover(token: str, user_key: str, message: str, priority = 0: int) -> bool:
+	"""
+	Send pushover _message_ to _user_.
+
+	Output: True if successful, else False
+	"""
+	try:
+		# Open connection
+		with http.client.HTTPSConnection("api.pushover.net:443") as conn:
+			conn.request("POST", "/1/messages.json",
+			urllib.parse.urlencode({
+				"token": token,
+				"user": user_key,
+				"message": message,
+				"priority": priority
+			}), { "Content-type": "application/x-www-form-urlencoded" })
+			result = conn.getresponse()
+		# Confirm success
+		assert result.code == 200
+	except:
+		print('Error occured sending pushover with the following params:')
+		print(f'message: {message}')
+		return False
+	return True
 
