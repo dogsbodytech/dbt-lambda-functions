4c485963fb2236597a37d15a0b825cfb7dd07198
Rob Hooper <rob@dogsbody.com>
2023-09-12 14:18:32 +0100

    fix: http.client does not support with

--- includes/Pushover.py
+++ includes/Pushover.py
@@ -12,19 +12,21 @@ def send_pushover(token: str, user_key: str, message: str, priority: int = 0) ->
 	"""
 	try:
 		# Open connection
-		with http.client.HTTPSConnection("api.pushover.net:443") as conn:
-			conn.request("POST", "/1/messages.json",
-			urllib.parse.urlencode({
-				"token": token,
-				"user": user_key,
-				"message": message,
-				"priority": priority
-			}), { "Content-type": "application/x-www-form-urlencoded" })
-			result = conn.getresponse()
+		conn = http.client.HTTPSConnection("api.pushover.net:443")
+		conn.request("POST", "/1/messages.json",
+		urllib.parse.urlencode({
+			"token": token,
+			"user": user_key,
+			"message": message,
+			"priority": priority
+		}), { "Content-type": "application/x-www-form-urlencoded" })
+		result = conn.getresponse()
+		conn.close()
 		# Confirm success
 		assert result.code == 200
-	except:
+	except Exception as err:
 		print(f'Error: Pushover failed with following params:\n{token},\n{user_key},\n{message},\n{priority}')
+		print(f'Error: Response {err}')
 		return False
 	return True
 
