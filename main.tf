56e01d563391c68d844e16d018827479571730c8
Dan Benton <dan@dogsbody.com>
2023-10-20 09:12:49 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -15,17 +15,17 @@ terraform {
 
 provider "aws" {
   region = "eu-west-2"
-  alias = "main"
+  alias  = "main"
 }
 
 provider "aws" {
   region = "us-east-1"
-  alias = "useast1"
+  alias  = "useast1"
 }
 
 resource "aws_api_gateway_rest_api" "api" {
-  name = "dbt-lambda-functions"
-  description = "The Dogsbody Technology Lamdbda Functions"
+  name                         = "dbt-lambda-functions"
+  description                  = "The Dogsbody Technology Lamdbda Functions"
   disable_execute_api_endpoint = true
   endpoint_configuration {
     types = ["EDGE"]
@@ -33,7 +33,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_acm_certificate" "cert" {
-  provider = aws.useast1
+  provider          = aws.useast1
   domain_name       = "webhook03.dogsbody.com"
   validation_method = "DNS"
   lifecycle {
@@ -42,16 +42,16 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_api_gateway_domain_name" "domain" {
-  domain_name = "webhook03.dogsbody.com"
-  certificate_arn = "${aws_acm_certificate.cert.id}"
+  domain_name     = "webhook03.dogsbody.com"
+  certificate_arn = aws_acm_certificate.cert.id
   endpoint_configuration {
     types = ["EDGE"]
   }
 }
 
 resource "aws_api_gateway_base_path_mapping" "base_path_mapping" {
-  api_id      = "${aws_api_gateway_rest_api.api.id}"
-  domain_name = "${aws_api_gateway_domain_name.domain.domain_name}"
+  api_id      = aws_api_gateway_rest_api.api.id
+  domain_name = aws_api_gateway_domain_name.domain.domain_name
 }
 
 
